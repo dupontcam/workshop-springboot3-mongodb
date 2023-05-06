c8926f1ba95d5e96f7b2e856c8bd6aff78b1e5fb
Carlos Dupont <dupontcam@gmail.com>
2023-05-06 11:32:23 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/dupontcam/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.dupontcam.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
