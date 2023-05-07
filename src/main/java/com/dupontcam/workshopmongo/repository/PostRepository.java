9b11e65d1d4d04dc6205191ca84f1fb86a675b5c
Carlos Dupont <dupontcam@gmail.com>
2023-05-07 15:25:26 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/dupontcam/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.dupontcam.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dupontcam.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
